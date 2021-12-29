a3be163c7a8936b2e1cc6fb70f554350e5111ac7
tcollenghi <tulio.collenghi@gmail.com>
2021-12-29 17:15:21 -0300

    Add files via upload

---
+++ Performance Tool - Copy.cmd
@@ -0,0 +1,94 @@
+@echo off
+rem Este es el titulo.
+title Performance Tool
+
+rem Estos son los servicios que se van a desabilitar.
+
+echo Welcome to Performance Tool!  
+timeout /t 3 /nobreak>nul
+cls
+echo Computer process optimization is about to start! 
+timeout /t 3 /nobreak>nul
+cls
+
+echo Optimizing services... 
+timeout /t 3 /nobreak>nul
+
+C:\WINDOWS\system32\svchost.exe -k LocalServiceNetworkRestricted -p
+
+rem for windows superfetch
+net stop "superfetch" 
+
+C:\WINDOWS\System32\svchost.exe -k utcsvc -p
+
+rem for windows index
+net stop "windows search" 
+
+C:\WINDOWS\System32\svchost.exe -k LocalSystemNetworkRestricted -p
+
+rem for Fax services
+net stop "fax"
+
+C:\WINDOWS\system32\svchost.exe -k netsvcs -p
+
+rem Adobe Acobat Updater Service
+net stop "AdobeARMservice" 
+
+C:\WINDOWS\System32\svchost.exe -k NetworkService -p
+
+rem Contact Data11b1d7e
+net stop "PimIndexMaintenanceSvc_11b1d7e" 
+
+C:\WINDOWS\System32\svchost.exe -k LocalSystemNetworkRestricted -p
+
+rem for XBOX
+net stop "XboxGipSvc" 
+
+C:\WINDOWS\system32\svchost.exe -k LocalService
+
+rem for XBOX
+net stop "xbgm"
+
+rem for XBOX
+net stop "XblAuthManager" 
+
+C:\WINDOWS\system32\svchost.exe -k LocalSystemNetworkRestricted
+
+rem for XBOX
+net stop "XblGameSave" 
+
+C:\WINDOWS\system32\svchost.exe -k localService -p
+
+rem For Demo Applications
+net stop "RetailDemo" 
+
+C:\WINDOWS\system32\svchost.exe -k netsvcs -p
+
+pause
+cls
+
+
+rem Esta seccion borra los archivos temporales de del folder temp.
+echo We will now proceed cleaning temporary files...
+timeout /t 3 /nobreak>nul
+del C:\Users\%username%\AppData\Local\Temp /f /s /q
+rd C:\Users\%username%\AppData\Local\Temp /s /q
+del c:\Windows\Temp /f /s /q
+rd c:\Windows\Temp /s /q
+del /q/f/s %TEMP%\*
+
+cls
+
+echo We will now synchronize the time with the server...
+timeout /t 3 /nobreak>nul
+net time \\129.6.15.28 /set /y
+
+timeout /t 3 /nobreak>nul
+
+echo Thank you for holding.
+timeout /t 3 /nobreak>nul
+
+cls
+
+echo Your PC should be working fine by now.
+timeout /t 3 /nobreak>nul
